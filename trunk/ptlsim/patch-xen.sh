805af432a1a79f10ba259a9864e727ce080ef421
Matt T. Yourst <yourst@yourst.com>
2006-08-16 08:34:36 +0000

    Update Xen patches
    git-svn-id: http://www.ptlsim.org/repository@143 a642bfc9-230e-0410-ae9c-f4e00b44d080

--- trunk/ptlsim/patch-xen.sh
+++ trunk/ptlsim/patch-xen.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 ln -sfv $PWD/xc_ptlsim.c $1/tools/libxc
 ln -sfv $PWD/xc_ptlsim.h $1/tools/libxc
-patch -p1 -d $1 < ptlsim-xen-3.x.diff 
+patch -p1 -d $1 < ptlsim-xen-hypervisor.diff 
+patch -p1 -d $1 < ptlsim-xen-tools.diff 
+patch -p1 -d $1 < xen-3.x-k8-cpufreq.diff
