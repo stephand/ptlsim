290abdb57d2880d3d2a0ede7a0b3c7646769dcc5
Matt T. Yourst <yourst@yourst.com>
2006-10-22 01:54:58 +0000

    - Make rdtsc into ptlcall_rdtsc to avoid name clash
    git-svn-id: http://www.ptlsim.org/repository@192 a642bfc9-230e-0410-ae9c-f4e00b44d080

--- trunk/ptlsim/ptlcalls.h
+++ trunk/ptlsim/ptlcalls.h
@@ -30,7 +30,7 @@ typedef W32 Waddr;
 #endif
 #endif // !__INSIDE_PTLSIM
 
-static inline W64 rdtsc() {
+static inline W64 ptlcall_rdtsc() {
   W32 lo, hi;
   asm volatile("rdtsc" : "=a" (lo), "=d" (hi));
   return ((W64)lo) | (((W64)hi) << 32);
